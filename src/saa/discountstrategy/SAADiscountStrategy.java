f58c3427a329a16bad03c0567c8758e9697a45fa
Gladwin <Gladwin@Gladwin-THINK>
2016-02-23 15:10:15 -0600

    DiscountStrategy first commit

---
+++ src/saa/discountstrategy/SAADiscountStrategy.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package saa.discountstrategy;
+
+/**
+ *
+ * @author Gladwin
+ */
+public class SAADiscountStrategy {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       // KLUDGE: do configuration here
+       
+       
+       
+       
+       // Start talking to objects
+    }
+    
+}
